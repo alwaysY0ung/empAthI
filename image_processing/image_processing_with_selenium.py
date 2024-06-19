4421439c53d4a1f89205e7ef47bbd1c5b5e1bd5d
eunseo0422 <144988420+eunseo0422@users.noreply.github.com>
2024-06-20 02:34:18 +0900

    #28 #24 #29 Selenium을 이용하여 이미지를 로드하는 코드에 이미지 캡셔닝을 수행하는 코드를 추가하였습니다.

--- image_processing/image_processing_with_selenium.py
+++ image_processing/image_processing_with_selenium.py
@@ -1,3 +1,53 @@
+# Copyright 2024 kairess
+
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+
+ #     http://www.apache.org/licenses/LICENSE-2.0
+
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+
+# https://github.com/Redcof/vit-gpt2-image-captioning
+# https://github.com/huggingface
+
+from PIL import Image
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+
+# 모델 및 토크나이저 로드
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+def image_captioning(image_paths):
+    images = []
+    for image_path in image_paths:
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+        images.append(i_image)
+
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
+
 import os
 import requests
 
@@ -43,5 +93,12 @@ if not os.path.exists(save_folder):
 # 이미지 다운로드 및 저장
 img_path = download_image(target_image_url, save_folder)
 
+
+# 이미지 캡셔닝 함수 호출
+if img_path:
+    image_captions = image_captioning([img_path])
+    print(f"Image Captions: {image_captions}")
+
 # WebDriver 종료
 driver.quit()
+
