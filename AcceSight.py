858e68c9abbc88d0caed65dbe169fb30dab77ebc
정소윤 <163826084+SoyunJung@users.noreply.github.com>
2024-06-19 21:02:59 +0900

    #23 gTTS 함수 완성했습니다. 라이센스도 기록하여 commit합니다.

--- AcceSight.py
+++ AcceSight.py
@@ -1,5 +1,5 @@
 """
-MediaPipe model usage
+MediaPipe
 Copyright 2024 kairess
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,32 @@ https://github.com/kairess/gesture-recognition
 위 repository의 test.py 코드와 robot.py를 일부 수정했습니다. 수정 사항은 주석으로 표시했습니다.
 I made some modifications to the code in test.py. The changes are indicated with comments.
 """
+"""
+gTTS
+MIT License
 
+Copyright (c) 2024 Pierre Nicolas Durette
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+https://github.com/pndurette/gTTS/tree/main의 tts.py를 참고해 구현한 코드입니다. gTTS 사용법 예제 코드.
+"""
 
 
 from selenium import webdriver
